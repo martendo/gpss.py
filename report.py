2751b7fab79e8a137f44178d6cc4d092586c018a
martendo <51766598+martendo@users.noreply.github.com>
2021-06-06 18:37:07 -0400

    Fix newlines in simulation report's entity sections

--- report.py
+++ report.py
@@ -3,13 +3,15 @@ from datetime import datetime
 def createReport(simulation):
     queues = ""
     for queue in simulation.queues.values():
-        queues += f"""  {queue.name}
+        queues += f"""
+  {queue.name}
     MAXIMUM CONTENTS: {queue.max}
     CURRENT CONTENTS: {queue.contents}"""
     
     facilities = ""
     for facility in simulation.facilities.values():
-        facilities += f"""  {facility.name}
+        facilities += f"""
+  {facility.name}
     NUMBER ENTRIES: {facility.entries}
     AVAILBLE: {"no" if facility.is_in_use else "yes"}"""
     
@@ -18,8 +20,6 @@ Generated on {datetime.now().strftime("%A, %B %d, %Y at %H:%M:%S %Z")}
 
 END TIME: {simulation.time}
 
-FACILITIES: {len(simulation.facilities)}
-{facilities}
+FACILITIES: {len(simulation.facilities)}{facilities}
 
-QUEUES: {len(simulation.queues)}
-{queues}"""
+QUEUES: {len(simulation.queues)}{queues}"""
