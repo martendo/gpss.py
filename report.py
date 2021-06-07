24abd667f6735f458c7c7063a393ebfa0b108c23
martendo <51766598+martendo@users.noreply.github.com>
2021-06-07 15:43:03 -0400

    strip() simulation report generation date
    The timezone string is empty if the date is "naive", which would leave a
    trailing space in the string.

--- report.py
+++ report.py
@@ -32,7 +32,8 @@ def createReport(simulation):
     Available: {"yes" if storage.available else "no"}"""
     
     return f"""gpss.py Simulation Report - {simulation.parser.inputfile}
-Generated on {datetime.now().strftime("%A, %B %d, %Y at %H:%M:%S %Z")}
+Generated on {datetime.now().strftime("%A, %B %d, %Y at %H:%M:%S %Z")
+    .strip()}
 
 End time: {simulation.time}
 
