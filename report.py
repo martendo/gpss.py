63bf5c66e569405c027c0d68541e7b3f7b81af7f
martendo <51766598+martendo@users.noreply.github.com>
2021-06-07 15:04:59 -0400

    Make simulation reports more readable
      - DON'T SCREAM EVERYTHING
      - Put Entity names in quotation marks, followed by a colon
      - Separate Entities with a blank line

--- report.py
+++ report.py
@@ -4,37 +4,40 @@ def createReport(simulation):
     queues = ""
     for queue in simulation.queues.values():
         queues += f"""
-  {queue.name}
-    MAXIMUM CONTENT: {queue.max}
-    TOTAL ENTRIES: {queue.entries}
-    ZERO ENTRIES: {queue.zero_entries}
-    PERCENT ZEROS: {(queue.zero_entries / queue.entries * 100):.2f}%
-    CURRENT CONTENT: {queue.content}"""
+
+  "{queue.name}":
+    Maximum content: {queue.max}
+    Total entries: {queue.entries}
+    Zero entries: {queue.zero_entries}
+    Percent zeros: {(queue.zero_entries / queue.entries * 100):.2f}%
+    Current content: {queue.content}"""
     
     facilities = ""
     for facility in simulation.facilities.values():
         facilities += f"""
-  {facility.name}
-    ENTRIES: {facility.entries}
-    AVAILBLE: {"no" if facility.is_in_use else "yes"}"""
+
+  "{facility.name}":
+    Entries: {facility.entries}
+    Availble: {"no" if facility.is_in_use else "yes"}"""
     
     storages = ""
     for storage in simulation.storages.values():
         storages += f"""
-  {storage.name}
-    CAPACITY: {storage.capacity}
-    MAXIMUM USAGE: {storage.max}
-    ENTRIES: {storage.entries}
-    REMAINING: {storage.available}
-    AVAILABLE: {"yes" if storage.available else "no"}"""
+
+  "{storage.name}":
+    Capacity: {storage.capacity}
+    Maximum usage: {storage.max}
+    Entries: {storage.entries}
+    Remaining: {storage.available}
+    Available: {"yes" if storage.available else "no"}"""
     
     return f"""gpss.py Simulation Report - {simulation.parser.inputfile}
 Generated on {datetime.now().strftime("%A, %B %d, %Y at %H:%M:%S %Z")}
 
-END TIME: {simulation.time}
+End time: {simulation.time}
 
-FACILITIES: {len(simulation.facilities)}{facilities}
+Facilities: {len(simulation.facilities)}{facilities}
 
-QUEUES: {len(simulation.queues)}{queues}
+Queues: {len(simulation.queues)}{queues}
 
-STORAGES: {len(simulation.storages)}{storages}"""
+Storages: {len(simulation.storages)}{storages}"""
