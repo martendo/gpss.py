750c444259776d176e691decbbcb67c8bbd1323a
martendo <51766598+martendo@users.noreply.github.com>
2021-06-13 13:45:21 -0400

    Improve average Queue content calculation

--- gpss/queue.py
+++ gpss/queue.py
@@ -8,26 +8,30 @@ class Queue:
         self.content = 0
         self.entries = 0
         self.max_content = 0
+        self.utilization = 0
         self.zero_entries = 0
         self.transactions = {}
         
-        self.changes = []
+        self.last_change = 0
     
     def __repr__(self):
         return f"Queue({self.content})"
     
     def change(self):
-        self.changes.append((self.content, self.simulation.time))
+        self.utilization += (
+            (self.simulation.time - self.last_change) * self.content)
+        self.last_change = self.simulation.time
     
     def join(self, transaction, content):
+        self.change()
         self.content += content
         if self.content > self.max_content:
             self.max_content = self.content
         self.entries += content
         self.transactions[transaction] = self.simulation.time
-        self.change()
     
     def depart(self, transaction, content):
+        self.change()
         self.content -= content
         if self.content < 0:
             simulation_error(self.simulation.parser.infile,
@@ -41,4 +45,3 @@ class Queue:
             warn(self.simulation.parser.infile,
                 transaction.current_linenum, "Transaction DEPARTed "
                 f"Queue \"{self.name}\" without first joining it")
-        self.change()
--- gpss/report.py
+++ gpss/report.py
@@ -1,28 +1,11 @@
-from itertools import tee
-
-def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
-
 def createReport(simulation):
     queues = ""
     for queue in simulation.queues.values():
-        # Calculate average content
-        time_spanned = simulation.time - queue.changes[0][1]
-        contentdur = 0
-        for (content, time), (nextcontent, nexttime) in pairwise(queue.changes):
-            if nexttime is None:
-                nexttime = simulation.time
-            contentdur += content * (nexttime - time)
-        average_content = contentdur / time_spanned
-        
         queues += f"""
 
   "{queue.name}":
     Maximum content: {queue.max_content}
-    Average content: {average_content:.3f}
+    Average content: {(queue.utilization / simulation.time):.3f}
     Total entries: {queue.entries}
     Zero entries: {queue.zero_entries}
     Percent zeros: {(queue.zero_entries / queue.entries * 100):.2f}%
