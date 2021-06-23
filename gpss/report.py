50604942dcf32586ec9f5f073a712a64064f78f9
martendo <51766598+martendo@users.noreply.github.com>
2021-06-23 17:34:23 -0400

    Move away from multi-line string literals
    Unfortunately, they don't play very nice with indentation.

--- gpss/report.py
+++ gpss/report.py
@@ -7,53 +7,57 @@ def na(value, fstring):
         return "N/A"
 
 def createReport(simulation):
-    queues = ""
+    queues = []
     for queue in simulation.queues.values():
-        queues += f"""
-
-  "{queue.name}":
-    Maximum content: {queue.max_content}
-    Average content: {queue.average_content:.3f}
-    Total entries: {queue.entries}
-    Zero entries: {queue.zero_entries}
-    Percent zeros: {na(queue.fraction_zeros * 100, "{:.2f}%")}
-    Avg. time/Trans.: {na(queue.average_time, "{:.3f}")}
-    Current content: {queue.content}"""
+        queues.append(
+            "\n\n"
+            f"  \"{queue.name}\":\n"
+            f"    Maximum content: {queue.max_content}\n"
+            f"    Average content: {queue.average_content:.3f}\n"
+            f"    Total entries: {queue.entries}\n"
+            f"    Zero entries: {queue.zero_entries}\n"
+            f"    Percent zeros: {na(queue.fraction_zeros * 100, '{:.2f}%')}\n"
+            f"    Avg. time/Trans.: {na(queue.average_time, '{:.3f}')}\n"
+            f"    Current content: {queue.content}"
+        )
     
-    facilities = ""
+    facilities = []
     for facility in simulation.facilities.values():
-        facilities += f"""
-
-  "{facility.name}":
-    Avg. utilization: {(facility.average_utilization * 100):.2f}%
-    Entries: {facility.entries}
-    Avg. time/Trans.: {na(facility.average_time, "{:.3f}")}
-    Available: {"no" if facility.is_in_use else "yes"}"""
+        facilities.append(
+            "\n\n"
+            f"  \"{facility.name}\":\n"
+            f"    Avg. utilization: {(facility.average_utilization * 100):.2f}%\n"
+            f"    Entries: {facility.entries}\n"
+            f"    Avg. time/Trans.: {na(facility.average_time, '{:.3f}')}\n"
+            f"    Available: {'no' if facility.is_in_use else 'yes'}"
+        )
     
-    storages = ""
+    storages = []
     for storage in simulation.storages.values():
-        storages += f"""
-
-  "{storage.name}":
-    Capacity: {storage.capacity}
-    Average content: {storage.average_content:.3f}
-    Avg. utilization: {(storage.average_utilization * 100):.2f}%
-    Entries: {storage.entries}
-    Avg. time/Trans.: {na(storage.average_time, "{:.3f}")}
-    Maximum content: {storage.max_content}
-    Current content: {storage.content}
-    Remaining: {storage.available}
-    Available: {"yes" if storage.available else "no"}"""
+        storages.append(
+            "\n\n"
+            f"  \"{storage.name}\":\n"
+            f"    Capacity: {storage.capacity}\n"
+            f"    Average content: {storage.average_content:.3f}\n"
+            f"    Avg. utilization: {(storage.average_utilization * 100):.2f}%\n"
+            f"    Entries: {storage.entries}\n"
+            f"    Avg. time/Trans.: {na(storage.average_time, '{:.3f}')}\n"
+            f"    Maximum content: {storage.max_content}\n"
+            f"    Current content: {storage.content}\n"
+            f"    Remaining: {storage.available}\n"
+            f"    Available: {'yes' if storage.available else 'no'}"
+        )
     
-    return f"""
-{f" SIMULATION {simulation.current_number} ":=^72}
-
-Relative Clock: {simulation.rel_time}
-Absolute Clock: {simulation.time}
-
-Facilities: {len(simulation.facilities)}{facilities}
-
-Queues: {len(simulation.queues)}{queues}
-
-Storages: {len(simulation.storages)}{storages}
-"""
+    return (
+        "\n"
+        f"{f' SIMULATION {simulation.current_number} ':=^72}\n"
+        "\n"
+        f"Relative Clock: {simulation.rel_time}\n"
+        f"Absolute Clock: {simulation.time}\n"
+        "\n"
+        f"Facilities: {len(simulation.facilities)}{''.join(facilities)}\n"
+        "\n"
+        f"Queues: {len(simulation.queues)}{''.join(queues)}\n"
+        "\n"
+        f"Storages: {len(simulation.storages)}{''.join(storages)}\n"
+    )
