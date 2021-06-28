7a10c1b3d1885de947e93b2f255c3abb43a4c38e
martendo <51766598+martendo@users.noreply.github.com>
2021-06-27 20:29:16 -0400

    Add gpss.py testing page using gpss-server

---
+++ docs/test/index.md
@@ -0,0 +1,25 @@
+---
+title: Test gpss.py
+permalink: /test
+style: /test/style.css
+---
+
+# Test gpss.py
+
+## Program
+
+<div class="editor-container">
+  <div id="editor" class="editor"></div>
+</div>
+
+## Output
+
+<button id="simulateBtn">Simulate</button>
+
+~~~
+Your simulation report will show up here.
+~~~
+{:#response}
+
+<script src="{{ '/test/ace/ace.js' | relative_url }}" type="text/javascript" charset="utf-8"></script>
+<script src="{{ '/test/script.js' | relative_url }}" type="text/javascript" charset="utf-8"></script>
---
+++ docs/test/script.js
@@ -0,0 +1,50 @@
+const output = document.getElementById("response").getElementsByTagName("code")[0];
+
+const editor = ace.edit("editor");
+editor.setOptions({
+  useSoftTabs: true,
+  printMargin: false,
+  displayIndentGuides: false,
+  tabSize: 8,
+});
+editor.focus();
+
+function responseError(response) {
+  output.textContent = "An unknown response was received from the server." + "\n\n" + response;
+}
+
+document.getElementById("simulateBtn").addEventListener("click", () => {
+  const request = new XMLHttpRequest();
+  request.addEventListener("error", () => {
+    output.textContent = "An error occurred while transferring data with the server.";
+  });
+  request.addEventListener("load", () => {
+    try {
+      data = JSON.parse(request.responseText);
+    } catch (error) {
+      if (error instanceof SyntaxError) {
+        responseError(request.responseText);
+      }
+    }
+    switch (data["status"]) {
+      case "parser-error":
+        const errors = [];
+        for (const error of data["errors"]) {
+          errors.push(`ERROR: Parser error: Line ${error["linenum"]}:\n    ${error["message"]}`);
+        }
+        output.textContent = data["message"] + "\n\n" + errors.join("\n");
+        break;
+      case "simulation-error":
+        output.textContent = data["message"] + "\n\n" + `ERROR: Simulation error: Line ${data["error"]["linenum"]}:\n    ${data["error"]["message"]}`;
+        break;
+      case "success":
+        output.textContent = data["report"];
+        break;
+      default:
+        responseError(request.responseText);
+        break;
+    }
+  });
+  request.open("POST", "https://gpss-server.herokuapp.com");
+  request.send(editor.getValue());
+});
