b90919b1abcd88fbfb3e86c6a75fd3886c945be3
martendo <51766598+martendo@users.noreply.github.com>
2022-03-05 16:08:20 -0500

    Print warnings and errors to stderr

--- gpss/_helpers.py
+++ gpss/_helpers.py
@@ -1,3 +1,4 @@
+from sys import stderr
 from .error import ParserError, SimulationError, ExecutionWarning
 
 properties = {
@@ -8,9 +9,9 @@ properties = {
 }
 
 # Print a message if using the CLI
-def output(*args):
+def output(*args, **kwargs):
 	if properties["cli"]:
-		print(*args)
+		print(*args, **kwargs)
 
 # Print a debug message if they are enabled
 def debugmsg(*args):
@@ -19,21 +20,21 @@ def debugmsg(*args):
 
 # Print a warning
 def warn(filename, linenum, message):
-	output(f"WARNING: {filename}({linenum}):\n    {message}")
+	output(f"WARNING: {filename}({linenum}):\n    {message}", file=stderr)
 	warning = ExecutionWarning(filename, linenum, message)
 	properties["warnings"].append(warning)
 	properties["messages"].append(warning)
 
 # Print a parser error
 def parser_error(parser, message):
-	output(f"ERROR: Parser error: {parser.infile}({parser.linenum}):\n    {message}")
+	output(f"ERROR: Parser error: {parser.infile}({parser.linenum}):\n    {message}", file=stderr)
 	error = ParserError(parser.infile, parser.linenum, message)
 	parser.errors.append(error)
 	properties["messages"].append(error)
 
 # Print and raise a simulation error
 def simulation_error(filename, linenum, message):
-	output(f"ERROR: Simulation error: {filename}({linenum}):\n    {message}")
+	output(f"ERROR: Simulation error: {filename}({linenum}):\n    {message}", file=stderr)
 	error = SimulationError(filename, linenum, message)
 	properties["messages"].append(error)
 	raise error
