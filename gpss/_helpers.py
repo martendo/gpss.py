b6ec4a06c1e6fbd60435089792e0f288b0d36728
Pablo Mazzini <pmazzini@gmail.com>
2023-02-25 15:12:43 +0000

    lint: flake8

--- gpss/_helpers.py
+++ gpss/_helpers.py
@@ -2,44 +2,54 @@ from sys import stderr
 from .error import ParserError, SimulationError, ExecutionWarning
 
 properties = {
-	"cli": False,
-	"debug": False,
-	"warnings": [],
-	"messages": [],
+    "cli": False,
+    "debug": False,
+    "warnings": [],
+    "messages": [],
 }
 
 # Print out a formatted error message if using the CLI
+
+
 def printerr(prefix, filename, linenum, message):
-	if not properties["cli"]:
-		return
-	string = f"{prefix}: {filename}"
-	if linenum is not None:
-		string += f"({linenum})"
-	string += f":\n    {message}"
-	print(string, file=stderr)
+    if not properties["cli"]:
+        return
+    string = f"{prefix}: {filename}"
+    if linenum is not None:
+        string += f"({linenum})"
+    string += f":\n    {message}"
+    print(string, file=stderr)
 
 # Print a debug message if they are enabled and using the CLI
+
+
 def debugmsg(*args):
-	if properties["debug"] and properties["cli"]:
-		print("DEBUG:", *args)
+    if properties["debug"] and properties["cli"]:
+        print("DEBUG:", *args)
 
 # Print a warning
+
+
 def warn(filename, linenum, message):
-	printerr("WARNING", filename, linenum, message)
-	warning = ExecutionWarning(filename, linenum, message)
-	properties["warnings"].append(warning)
-	properties["messages"].append(warning)
+    printerr("WARNING", filename, linenum, message)
+    warning = ExecutionWarning(filename, linenum, message)
+    properties["warnings"].append(warning)
+    properties["messages"].append(warning)
 
 # Print a parser error
+
+
 def parser_error(parser, message):
-	printerr("ERROR: Parser error", parser.infile, parser.linenum, message)
-	error = ParserError(parser.infile, parser.linenum, message)
-	parser.errors.append(error)
-	properties["messages"].append(error)
+    printerr("ERROR: Parser error", parser.infile, parser.linenum, message)
+    error = ParserError(parser.infile, parser.linenum, message)
+    parser.errors.append(error)
+    properties["messages"].append(error)
 
 # Print and raise a simulation error
+
+
 def simulation_error(filename, linenum, message):
-	printerr("ERROR: Simulation error", filename, linenum, message)
-	error = SimulationError(filename, linenum, message)
-	properties["messages"].append(error)
-	raise error
+    printerr("ERROR: Simulation error", filename, linenum, message)
+    error = SimulationError(filename, linenum, message)
+    properties["messages"].append(error)
+    raise error
